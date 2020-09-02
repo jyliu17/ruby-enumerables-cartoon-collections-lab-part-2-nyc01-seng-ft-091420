1258c5009e1be32e62e9b10504bd802a0d92dafc
Johnson Liu <jyliu17@gmail.com>
2020-09-02 02:10:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,9 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  
+  call.length < 5
   planeteer_calls.select do |call|
-    call.length < 5 
+     call = true
     
   end
 end
